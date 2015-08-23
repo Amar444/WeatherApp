74b6f542ffcb062887a5ec9287a60dd191a70b6b
Amar <tursicamar@gmail.com>
2015-08-24 00:27:19 +0200

    Some layout changes, changed API link (now getting different information)

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ app.controller('WeatherController', [ '$http', function($http){
 
 	//paste the next line in key.js:
 	//var key = '4P1 K3Y H3R3'
-	var link = 'http://api.openweathermap.org/data/2.5/forecast/city?id=2755204&APPID=' + key;
+	var city_id = '2755204';
+	var link = 'http://api.openweathermap.org/data/2.5/weather?id='+ city_id +'&APPID=' + key;
 	
 	$http.get(link).success(function(data){
 		weather.information = data;
