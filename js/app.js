b5bb7623c715052f3b90b55a7c2cea9d04acbf5c
Amar <tursicamar@gmail.com>
2015-08-23 22:36:58 +0200

    Connected the Openweathermap API

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 var app = angular.module('myApp', []);
+
+app.controller('WeatherController', [ '$http', function($http){
+	var weather = this;
+	weather.information = [];
+
+	//paste the next line in key.js:
+	//var key = '4P1 K3Y H3R3'
+	var link = 'http://api.openweathermap.org/data/2.5/forecast/city?id=2755204&APPID=' + key;
+	
+	$http.get(link).success(function(data){
+		weather.information = data;
+	});
+} ]);
